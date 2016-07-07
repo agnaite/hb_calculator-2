eac91e0bf5757a2966d3dfc7aa3f33071f5cda03
Hackbright Student <info@hackbrightacademy.com>
2016-07-07 15:52:29 -0700

    addition and subtraction

---
+++ calculator.py
@@ -0,0 +1,20 @@
+"""
+calculator.py
+
+Using our arithmetic.py file from Exercise02, create the
+calculator program yourself in this file.
+"""
+
+from arithmetic import *
+
+input_string = None
+
+while input_string != "q":
+    input_string = raw_input(">>")
+    tokens = input_string.split(" ")
+    if tokens[0] == "+":
+        print add(int(tokens[1]),int(tokens[2]))
+    elif tokens[0] == "-":
+        print subtract(int(tokens[1]),int(tokens[2]))
+
+# Your code goes here
