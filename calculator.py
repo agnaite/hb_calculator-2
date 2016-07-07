1c6557a262eb82f410c1a46e6cffbc61dbbb4899
Hackbright Student <info@hackbrightacademy.com>
2016-07-07 15:55:22 -0700

    added multiplication

--- calculator.py
+++ calculator.py
@@ -10,11 +10,13 @@ from arithmetic import *
 input_string = None
 
 while input_string != "q":
-    input_string = raw_input(">>")
+    input_string = raw_input(">> ")
     tokens = input_string.split(" ")
     if tokens[0] == "+":
         print add(int(tokens[1]),int(tokens[2]))
     elif tokens[0] == "-":
         print subtract(int(tokens[1]),int(tokens[2]))
+    elif tokens[0] == "*":
+        print multiply(int(tokens[1]), int(tokens[2]))
 
 # Your code goes here
