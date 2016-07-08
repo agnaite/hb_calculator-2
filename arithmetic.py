35ea1519d1febec26fd1aaa5de5faabafa17d573
Hackbright Student <info@hackbrightacademy.com>
2016-07-07 17:56:55 -0700

    now using reduce()

--- arithmetic.py
+++ arithmetic.py
@@ -1,26 +1,18 @@
 def add(numlist):
-    sum = 0
-    for x in numlist:
-        sum = sum + x
+    sum = reduce (lambda x, y: x + y, numlist)
     return sum
 
 def subtract(numlist):
-    difference = numlist.pop(0)
-    for x in numlist:
-        difference = difference - x
+    difference = reduce (lambda x, y: x - y, numlist)
     return difference
 
 def multiply(numlist):
-    product = 1
-    for x in numlist:
-        product *= x
+    product = reduce (lambda x, y: x * y, numlist)
     return product
 
 def divide(numlist):
-    product = numlist.pop(0)
-    for x in numlist:
-        product /= float(x)
-    return product
+    quotient = reduce (lambda x, y: x / y, numlist)
+    return quotient
 
 def square(numlist):
     # Needs only one argument
@@ -32,7 +24,6 @@ def cube(numlist):
 
 def power(numlist):
     return numlist[0] ** numlist[1]  # ** = exponent operator
-
 
 def mod(numlist):
     return numlist[0] % numlist[1]
